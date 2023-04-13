aa62fbaceba377559e9492e68cd3a56b2bd88924
bdg <max>
2023-04-13 18:31:42 +0800

    init

--- src/src/pages/Home/Home.js
+++ src/src/pages/Home/Home.js
@@ -18,7 +18,7 @@ function Home () {
             <Button key={1} sx={{ my: 2, color: 'white', display: 'block' }} onClick={() => { navigate('/') }}>首页</Button>
             <Button key={2} sx={{ my: 2, color: 'white', display: 'block' }} onClick={() => { navigate('/tutor/html') }}>Html</Button>
             <Button key={3} sx={{ my: 2, color: 'white', display: 'block' }} onClick={() => { navigate('/tutor/css') }}>Css</Button>
-            <Button key={3} sx={{ my: 2, color: 'white', display: 'block' }} onClick={() => { navigate('/tutor/effect') }}>Effect</Button>
+            <Button key={4} sx={{ my: 2, color: 'white', display: 'block' }} onClick={() => { navigate('/tutor/effect') }}>Effect</Button>
           </Stack>
         </Box>
       </>)}
--- src/src/pages/Tutor/Effect.js
+++ src/src/pages/Tutor/Effect.js
@@ -19,98 +19,65 @@ import {
 } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 
-function TabPanel (props) {
-  const { children, value, index, ...other } = props
-
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`action-tabpanel-${index}`}
-      aria-labelledby={`action-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
-    </Typography>
-  )
+let styleBorder = {
+  border: '1px solid black',
+  margin: '10px',
+  padding: '3px',
 }
 
-const fabStyle = {
-  position: 'absolute',
-  bottom: 16,
-  right: 16,
-}
-
-const fabGreenStyle = {
-  color: 'white',
-  bgcolor: green[500],
-  '&:hover': {
-    bgcolor: green[600],
-  },
-}
 
 function FloatingActionButtonZoom () {
   const theme = useTheme()
-  const [value, setValue] = React.useState(0)
+  const [curIdx, setCurIdx] = React.useState(0)
+
+  function TabPanel ({ children, index, ...other }) {
+    return (
+      <Typography
+        component="div"
+        {...other}
+      >
+        <Box sx={{ p: 3 }}>{children}</Box>
+      </Typography>
+    )
+  }
+
+  const fabStyle = {
+    position: 'absolute',
+    bottom: 16,
+    right: 16,
+  }
+
+  const fabGreenStyle = {
+    color: 'white',
+    bgcolor: green[500],
+    '&:hover': {
+      bgcolor: green[600],
+    },
+  }
 
   return (
-    <Box
-      sx={{
-        bgcolor: 'background.paper',
-        width: 500,
-        position: 'relative',
-        minHeight: 200,
-      }}
+    <Box position='relative' sx={{ width: 500, minHeight: 200, ...styleBorder, }}
     >
       <AppBar position="relative" color="default">
-        <Tabs
-          value={value}
-          onChange={(event, newValue) => setValue(newValue)}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="fullWidth"
-        >
-          <Tab label="Item One" />
-          <Tab label="Item Two" />
-          <Tab label="Item Three" />
+        <Tabs value={curIdx} variant="fullWidth" onChange={(event, newValue) => { setCurIdx(newValue) }}>
+          {['Item One', 'Item Two', 'Item Three'].map((item, idx) => (<Tab label={item} key={idx}></Tab>))}
         </Tabs>
       </AppBar>
-      <SwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={value}
-        onChangeIndex={(index) => setValue(index)}
-      >
-        {['Item One', 'Item Two', 'Item Three'].map((item, idx) => (
-          <TabPanel value={value} index={idx} dir={theme.direction}>{item}</TabPanel>
-        ))}
-      </SwipeableViews>
-      {
-        [
-          { color: 'primary', sx: fabStyle, icon: <AddIcon />, },
-          { color: 'secondary', sx: fabStyle, icon: <EditIcon />, },
-          { color: 'inherit', sx: { ...fabStyle, ...fabGreenStyle }, icon: <UpIcon />, },
-        ].map((fab, index) => (
-          <Zoom
-            key={fab.color}
-            in={value === index}
-            timeout={{ enter: 500, exit: 200 }}
-            sx={{
-              bottom: `20px`,
-              right: `20px`,
-              position: 'absolute'
-            }}
-          >
-            <Fab sx={fab.sx} color={fab.color}>
-              {fab.icon}
-            </Fab>
-          </Zoom>
-        ))}
+      <SwipeableViews index={curIdx} axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        children={['Item One', 'Item Two', 'Item Three'].map((item, idx) => (<TabPanel index={idx} dir={theme.direction} key={idx}>{item}</TabPanel>))}
+      />
+      {[
+        { color: 'primary', sx: fabStyle, icon: <AddIcon />, },
+        { color: 'secondary', sx: fabStyle, icon: <EditIcon />, },
+        { color: 'inherit', sx: { ...fabStyle, ...fabGreenStyle }, icon: <UpIcon />, },
+      ].map((item, idx) => (
+        <Zoom key={idx} in={curIdx === idx} timeout={{ enter: 500, exit: 200 }} sx={item.sx} >
+          <Fab color={item.color}> {item.icon} </Fab>
+        </Zoom>
+      ))}
     </Box>
   )
 }
-
-
 
 function Effect () {
   const navigate = useNavigate()
@@ -124,11 +91,16 @@ function Effect () {
             <Button key={1} sx={{ my: 2, color: 'white', display: 'block' }} onClick={() => { navigate('/') }}>首页</Button>
             <Button key={2} sx={{ my: 2, color: 'white', display: 'block' }} onClick={() => { navigate('/tutor/html') }}>Html</Button>
             <Button key={3} sx={{ my: 2, color: 'white', display: 'block' }} onClick={() => { navigate('/tutor/css') }}>Css</Button>
-            <Button key={3} sx={{ my: 2, color: 'white', display: 'block' }} onClick={() => { navigate('/tutor/effect') }}>Effect</Button>
+            <Button key={4} sx={{ my: 2, color: 'white', display: 'block' }} onClick={() => { navigate('/tutor/effect') }}>Effect</Button>
           </Stack>
         </Box>
       </>)}
       main={(<>
+        <pre id="thanks">
+          <div>感谢 mozilla w3c</div>
+          <div>参考于 <a href='https://developer.mozilla.org/zh-CN/docs/Web/HTML/Element/a' target="_blank" rel="noreferrer">https://developer.mozilla.org/zh-CN/docs/Web/HTML/Element/a</a></div>
+          <div>参考于 <a href="https://www.w3school.com.cn/cssref/pr_align-items.asp" target="_blank" rel="noreferrer">https://www.w3school.com.cn/cssref/pr_align-items.asp</a></div>
+        </pre>
         <FloatingActionButtonZoom />
       </>)}
     />

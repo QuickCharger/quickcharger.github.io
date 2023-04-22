174fdfe6f83e1c0c071dbcf34ecaaec0c12cb9bf
bdg <max>
2023-04-22 13:50:17 +0800

    init

--- src/src/pages/Layout/Template_Persistent.js
+++ src/src/pages/Layout/Template_Persistent.js
@@ -1,79 +1,24 @@
 import * as React from 'react'
 import {
-  AppBar as MuiAppBar,
+  AppBar,
   Drawer,
   IconButton,
-  Box, Toolbar,
+  Box, Toolbar, Collapse,
+  Stack, Grid,
 } from '@mui/material'
 import { styled } from '@mui/material/styles'
 import MenuIcon from '@mui/icons-material/Menu'
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import { useTheme } from '@mui/material/styles'
 
 const drawerWidth = 240
 
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: `-${drawerWidth}px`,
-    ...(open && {
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      marginLeft: 0,
-    }),
-  }),
-)
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}))
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-  padding: theme.spacing(0, 1),
-  ...theme.mixins.toolbar,
-}))
-
 export default function Template ({ header, sider, main, footer }) {
+  const [openCollapse, setOpenCollapse] = React.useState(true)
 
-  const [openDrawer, setOpenDrawer] = React.useState(true)
-
-  return (
-    <Box sx={{ display: 'flex' }}>
-      <AppBar open={openDrawer}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            onClick={() => setOpenDrawer(!openDrawer)}
-            sx={{ mr: 2, ...(openDrawer && { display: 'none' }) }}
-          >
-            <MenuIcon />
-          </IconButton>
-          {header}
-        </Toolbar>
-      </AppBar>
-
+  const theme = useTheme()
+  return (<>
+    <Stack direction='row' id='stackroot' alignItems='stretch'>
       <Drawer
         display='flex'
         flex-basis={drawerWidth}
@@ -82,20 +27,52 @@ export default function Template ({ header, sider, main, footer }) {
           width: drawerWidth,
           [`& .MuiDrawer-paper`]: { width: drawerWidth, },
         }}
-        open={openDrawer}
+        open={openCollapse}
+        unmountOnExit
       >
-        <DrawerHeader>
-          <IconButton onClick={() => setOpenDrawer(!openDrawer)}>
+        <Stack direction='row' justifyContent='flex-end' alignItems='center' sx={{ height: '64px' }}>
+          <IconButton sx={{ mr: '10px' }} onClick={() => setOpenCollapse(!openCollapse)}>
             <ChevronLeftIcon />
           </IconButton>
-        </DrawerHeader>
+        </Stack>
         {sider}
       </Drawer>
 
-      <Main open={openDrawer} component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <Toolbar />
-        {main}
-      </Main>
-    </Box >
-  )
+      <Box id='box' sx={{
+        width: '100%',
+        ...(openCollapse && {
+          transition: theme.transitions.create('margin-left', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+          }),
+          marginLeft: 0,
+        }),
+        ...(!openCollapse && {
+          transition: theme.transitions.create('margin-left', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+          }),
+          marginLeft: `-${drawerWidth}px`,
+        }),
+      }}>
+        <Stack direction='column' id='stack' sx={{}}>
+          <AppBar open={openCollapse} id='appbar' position='sticky'>
+            <Toolbar>
+              <Collapse in={!openCollapse} orientation='horizontal' unmountOnExit>
+                <IconButton color="inherit" sx={{ mr: 2 }} onClick={() => setOpenCollapse(!openCollapse)} >
+                  <MenuIcon />
+                </IconButton>
+              </Collapse>
+              {header}
+            </Toolbar>
+          </AppBar>
+
+          <Box sx={{ marginLeft: '20px' }}>
+            <Toolbar />
+            {main}
+          </Box>
+        </Stack>
+      </Box>
+    </Stack >
+  </>)
 }
